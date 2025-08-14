b2c404c27814418d6d6a6b00583644b52a6b178d
oleksandrboo77 <oleksandrboo77@gmail.com>
2025-08-14 14:33:51 +0300

    add placeholder and useeffect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import MovieGrid from "../MovieGrid/MovieGrid";
 import SearchBar from "../SearchBar/SearchBar";
-import { Toaster } from "react-hot-toast";
-import { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import { useState, useEffect } from "react";
 import type { Movie } from "../../types/movie";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import { getMovies } from "../../services/movieService";
 import MovieModal from "../MovieModal/MovieModal";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import Pagination from "../Pagination/Pagination";
 
 export default function App() {
@@ -19,10 +19,17 @@ export default function App() {
     queryKey: ["movies", searchQuery, currentPage],
     queryFn: () => getMovies(searchQuery, currentPage),
     enabled: !!searchQuery,
+    placeholderData: keepPreviousData,
   });
 
   const movies = data?.results ?? [];
   const totalPages = data?.total_pages ?? 0;
+
+  useEffect(() => {
+    if (isSuccess && movies.length === 0) {
+      toast.error("No movies found for your request");
+    }
+  }, [isSuccess, movies]);
 
   const handleSearch = (newQuery: string) => {
     setSearchQuery(newQuery);
@@ -45,8 +52,6 @@ export default function App() {
 
       {isLoading && <Loader />}
       {isError && <ErrorMessage error={error} />}
-
-      {isSuccess && movies.length === 0 && <div>No movies found</div>}
 
       {movies.length > 0 && (
         <MovieGrid movies={movies} onSelect={handleSelectMovie} />
